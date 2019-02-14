286f651b89b379aa476c795cc089f305582a15c9
renefibonacci660 <660@holbertonschool.com>
2019-02-14 18:09:37 +0000

    Adding 2, first commit

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ * @a: holds first value given in 2-main.c
+ * @b: holds second value given in 2-main.c
+ * Return: Completed value of mutiplied integers
+**/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
