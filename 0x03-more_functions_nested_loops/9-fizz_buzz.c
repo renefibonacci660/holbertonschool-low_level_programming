4ffbdc2f3956cd6f1fb42e39acea84069738a321
renefibonacci660 <660@holbertonschool.com>
2019-02-15 05:14:12 +0000

    Adding 9 with a for loop

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else if (n % 15 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", n);
+		if (n < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
