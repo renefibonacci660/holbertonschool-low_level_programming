10762d563a1fe88a5a223ea9da6e02a39d2be9aa
renefibonacci660 <660@holbertonschool.com>
2019-02-20 00:10:41 +0000

    Testing 7, third commit

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -9,23 +9,20 @@ void puts_half(char *str)
 	int count = 0;
 	int startp;
 
-	for (; str[count]; count++)
+	for (; str[count] != '\0'; count++)
 	{}
 	if (count % 2 == 0)
 	{
 		startp = count / 2;
-		for (; startp <= count; startp++)
-	{
-		_putchar(str[startp]);
-	}
 	}
 	else
 	{
 		startp = (count + 1) / 2;
-		for (; startp <= count; startp++)
+	}
+
+	for (; str[startp] != '\0'; startp++)
 	{
 		_putchar(str[startp]);
-	}
 	}
 	_putchar('\n');
 }
