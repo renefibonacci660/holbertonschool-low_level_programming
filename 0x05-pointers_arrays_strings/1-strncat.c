7356ca76a520f7f21b177ed146779bc3342ac39b
renefibonacci660 <660@holbertonschool.com>
2019-02-21 21:03:53 +0000

    Modified 1, second commit

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -15,12 +15,12 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (len1 = 0; dest[len1]; len1++)
 	{}
-	for (len2 = 0; len2 + 1 < n && src[len2]; len1++, len2++)
+	for (len2 = 0; len2 < n && src[len2]; len1++, len2++)
 	{
-		dest[len1] = dest[len1] + src[len2];
+		dest[len1] = src[len2];
 	}
 
-	dest[len1] = src[len2]; /** adds null character **/
+	dest[len1] = '\0';
 
 	return (dest);
 }
