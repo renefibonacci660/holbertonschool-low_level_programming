9d61df3a6068a93507e4b52a74f67bc5fd3e1725
renefibonacci660 <660@holbertonschool.com>
2019-02-25 22:19:00 +0000

    Adding 2, first commit

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strchr - function locates the first occurrence of c in the string
+ * @s:pointer to string assigned in 2-main.c
+ * @c: character 'l' we are checking for in string
+ * Return: string from c on or null if c not present
+**/
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
