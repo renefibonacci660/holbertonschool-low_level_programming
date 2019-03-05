a6218cb12b39c80a1572b808e8705306decd4653
renefibonacci660 <660@holbertonschool.com>
2019-03-05 21:15:38 +0000

    Addingnew folder with readme, header and 0, first commit

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ate_array - Creates space in memory for an array
+ * @size: size of array assigned in 0-main.c
+ * @c: character that initilizes array assigned in 0-main.c
+ * Return: pointer to the allocated memory space, NULL if size is null
+**/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p;
+
+	if (size == 0)
+		return (NULL);
+	p = malloc(size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
---
+++ 0x0A-malloc_free/README.md
@@ -0,0 +1,6 @@
+In this project we are attempting to answer the following questions
+
+What is the difference between automatic and dynamic allocation
+What is malloc and free and how to use them
+Why and when use malloc
+How to use valgrind to check for memory leak
