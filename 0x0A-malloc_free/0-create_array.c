ab5eba045f93642909dad1556608c1ac2ecd9dd5
renefibonacci660 <660@holbertonschool.com>
2019-03-05 21:31:55 +0000

    Trying to make 0 cleaner, third commit

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -13,10 +13,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *p;
 
-	if (size == 0)
-		return (NULL);
 	p = malloc(size);
-	if (p == NULL)
+	if (size == 0 || p == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
