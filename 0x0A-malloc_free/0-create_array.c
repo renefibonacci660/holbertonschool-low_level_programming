e112df01c205f4be77786ec604dc7e9fb0930c06
renefibonacci660 <660@holbertonschool.com>
2019-03-05 21:20:16 +0000

    0 now passing betty, second commit

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -16,7 +16,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	p = malloc(size);
-	
+	if (p == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
