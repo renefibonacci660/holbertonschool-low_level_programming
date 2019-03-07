ce5080af1ac32b9031b230bc6149abd9e2cca0cd
renefibonacci660 <660@holbertonschool.com>
2019-03-07 23:44:23 +0000

    Modified 1, third commit

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -16,19 +16,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int element;
 	unsigned int count;
 
-	if (!s1)
-		s1 = "";
-	
-	if (!s2)
-		s2 = "";
-
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 	for (len1 = 0; s1[len1]; len1++)
 	{}
 
-	for (len2 = 0; len2 <= n; len2++)
+	for (len2 = 0; len2 < n; len2++)
 	{}
 
 	p = malloc((len1 + len2) * sizeof(char) + 1);
