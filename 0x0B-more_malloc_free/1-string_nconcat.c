50fb5e80835290ec7c31e1e4ebad96b5bfae473a
renefibonacci660 <660@holbertonschool.com>
2019-03-08 01:24:24 +0000

    Modified if condition for 1, fourth commit

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -16,14 +16,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int element;
 	unsigned int count;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	for (len1 = 0; s1[len1]; len1++)
 	{}
 
 	for (len2 = 0; len2 < n; len2++)
 	{}
+	if (len2 >= n)
 
 	p = malloc((len1 + len2) * sizeof(char) + 1);
 
