7f02ae5cba66425c02c655fb9c84a0be2576598d
renefibonacci660 <660@holbertonschool.com>
2019-03-08 07:40:59 +0000

    Adding 2 and 3

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -2,11 +2,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * _calloc -
- *
- * Return: 
+ * _calloc - memory allocates for array
+ * @nmemb: number of array values
+ * @size: size of each individual array value
+ * Return: A pointer to the beginning of array or NULL if NULL array
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
+	char *p;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0;  i < nmemb * size; i++)
+		p[i] = 0;
+
+	return (p);
 }
--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * array_range -
- *
- * Return: 
+ * array_range - creates an array of integers
+ * @min: min of size
+ * @max: max of elements
+ * Return: NULL if min > max or pointer to the newly created array
  */
 int *array_range(int min, int max)
 {
+	int length;
+	int *p;
+	int counter;
 
+	if (min > max)
+		return (NULL);
+
+	length = max - min + 1;
+
+	p = malloc(sizeof(int) + length);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (counter = 0; counter < length; counter++)
+		p[counter] = min++;
+
+	return (p);
 }
