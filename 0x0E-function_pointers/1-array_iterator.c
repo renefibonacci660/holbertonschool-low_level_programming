8c82488ecb9dc4de12a55db4e07dd98faea4c438
renefibonacci660 <660@holbertonschool.com>
2019-03-13 23:50:02 +0000

    adding 1, first commit

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * 
+ * array_iterator - iterates through array and calls function
+ * @array: array specified in 1-main.c
+ * @size: size of array specified in 1-main.c
+ * @action: pointer to function specified in 1-main.c
  **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
