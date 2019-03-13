811bc6d748845848de32ae792d1fd9fc9c7f7c42
renefibonacci660 <660@holbertonschool.com>
2019-03-13 22:30:31 +0000

    Adding new folder and 0, first commit

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - Calls a function to print a name
+ * @name: A pointer to a string
+ * @f: A pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	void (*p)(char *);
+
+	if (name == NULL || f == NULL)
+		return;
+	p = f;
+	p(name);
+}
