05e2007342f7f98b9896fddcf1690176b5a530e5
renefibonacci660 <660@holbertonschool.com>
2019-03-13 23:46:28 +0000

    adding 1, first commit

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,11 +1,15 @@
+#include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * array_iterator -
- *
- * Return: 
- */
+ * 
+ **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int count;
 
+	if (array == NULL || action == NULL)
+		return;
+
+	for (count = 0; count < size; count++)
+		action(array[count]);
 }
