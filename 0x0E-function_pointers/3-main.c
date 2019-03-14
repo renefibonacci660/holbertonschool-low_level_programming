b4ffe9855e7fc5b87e4e8f0924b42e4ffe96bca0
renefibonacci660 <660@holbertonschool.com>
2019-03-14 00:50:27 +0000

    Completed 3 and all files, first commit

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,11 +1,43 @@
-#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * op_add -
- *
- * Return: 
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Pointer to an array containing the parameters
+ * Return: Always zero
  */
-int op_add(int a, int b)
+int main(int argc, char **argv)
 {
+	int a;
+	int b;
+	char *sm;
+	int (*input)(int, int);
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	sm = argv[2];
+
+
+	if (*sm != '+' && *sm != '-' && *sm != '*' && *sm != '/' && *sm != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	input = get_op_func(sm);
+	if (input == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", input(a, b));
+	return (0);
 }
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - subtracts second number from first
+ * @a: first number
+ * @b: second number
+ * Return: completed subtraction
+**/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiplied numbers
+**/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - divides first number by second number
+ * @a: first number
+ * @b: second number
+ * Return: 100 if divisor is 0, completed division if divisor > 0
+**/
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return (a / b);
+}
+
+
+/**
+ * op_mod - provides remainder of a % b
+ * @a: first number
+ * @b: second number
+ * Return: 100 if second number is 0, or remainder of a % b if b > 0
+**/
+int op_mod(int a, int b)
+{
+		if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return (a % b);
+}
