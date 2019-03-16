6abf0048e5d27c73b821a9b003c961a013688365
renefibonacci660 <660@holbertonschool.com>
2019-03-16 21:42:51 +0000

    modified variable type, second commit

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,13 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
+	va_start(list, n);
+
 	if (n == 0)
 		return (0);
 
-	va_start(list, n);
-
 	for (i = 0; i < n; i++)
-		sum += va_arg(list, unsigned int);
+		sum += va_arg(list, int);
 
 	va_end(list);
 
