82865f90df5a93ebaad50cb44ca708adb99f04bc
renefibonacci660 <660@holbertonschool.com>
2019-03-16 22:01:55 +0000

    Changing where variable values are declared, 3rd commit

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -7,19 +7,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list list;
 	unsigned int i;
 	int sum;
-	va_list slist;
+
+	va_start(list, n);
 
 	sum = 0;
-
-	va_start(slist, n);
 
 	if (n == 0)
 		return (0);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(slist, int);
+		sum += va_arg(list, int);
+
+	va_end(list);
 
 	return (sum);
 }
