2b7b67c3be5a5214ffcf2db7f5365c131243dc0a
renefibonacci660 <660@holbertonschool.com>
2019-03-17 01:51:46 +0000

    Added putchar to header file

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -18,6 +18,7 @@ typedef struct op
 	void (*f)(va_list list);
 } op_t;
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
