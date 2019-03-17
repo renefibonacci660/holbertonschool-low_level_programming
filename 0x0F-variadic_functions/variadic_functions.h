032a7188226231aa1a0223dbd69b8eeedceaa97d
renefibonacci660 <660@holbertonschool.com>
2019-03-17 01:39:38 +0000

    Added struct to file for 3, second commit

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -3,6 +3,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct op - struct for function parameter to print anything
+ * passed; used rn by 3-print_all.c
+ * @op: the case use
+ * @f: the function to parse cases to determine what variable type to print
+ */
+
+typedef struct op
+{
+	char *s;
+	void (*f)(va_list list);
+} op_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
