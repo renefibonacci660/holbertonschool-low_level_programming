f3923bdbc39dc6ed9d565b5ff90a7a3d328b80ee
renefibonacci660 <660@holbertonschool.com>
2019-03-22 03:26:00 +0000

    Adding 2, passing style checker and first commit

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+int length(const char *str);
+/**
+ * add_node - creates a new node and adds it to the passed linked list
+ * @head: pointer to head of linked list
+ * @str: string assigned to new node
+ * Return: pointer to head of linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (!head)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
+		return (NULL);
+
+	if (!*head) /*initialzes the next pointer to NULL*/
+		new_node->next = NULL;
+	else /*moves new node to the head*/
+		new_node->next = *head;
+
+	new_node->str = strdup(str);
+	new_node->len = length(str);
+	*head = new_node;
+	return (*head);
+}
+
+/**
+ * len - calculates the length of string
+ * @str: constant string
+ * Return: length of string
+ */
+int length(const char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
