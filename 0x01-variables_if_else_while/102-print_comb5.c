0984f85a673a0cbcba674f3a2e5621e219c158f4
renefibonacci660 <660@holbertonschool.com>
2019-03-26 22:28:17 +0000

    Prints all possible combos of 2 2 digit numbers without repeat, first commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - calculates all possible combos two digit numbers without repeats
+ * Return: 0
+**/
+int main(void)
+{
+	int first = 0;
+	int second = 0;
+
+	while (first < 99)
+	{
+		first++;
+
+		for (second = first + 1; second <= 99; second++)
+		{
+			if (first < second)
+			{
+				putchar (first / 10 + 48);
+				putchar (first % 10 + 48);
+				putchar (' ');
+				putchar (second / 10 + 48);
+				putchar (second % 10 + 48);
+
+				if (first != 98)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
