fd9203f74a3e7cdfbed28f18443521ed1b96aeee
renefibonacci660 <660@holbertonschool.com>
2019-03-26 22:56:12 +0000

    Adding new folder and first problem that counts all elements of list

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints all elements of list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
