9dcdeeea4024236c03f0c4b5212f2192af9afc84
renefibonacci660 <660@holbertonschool.com>
2019-03-27 01:06:25 +0000

    frees lists of nodes, first commit

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+
+#include "lists.h"
+/**
+ * free_listint2 - frees list of nodes, sets the head to NULL
+ * @head: pointer to the first element of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	free(*head);
+	*head = NULL;
+}
