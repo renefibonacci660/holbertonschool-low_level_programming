8c0495aa5dbb83333a6195d40d58275044bd3f73
renefibonacci660 <660@holbertonschool.com>
2019-04-03 02:31:53 +0000

    Modified permission type given to created file, third commit

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	func = open(filename, O_CREAT | O_RDWR, 0600);
+	func = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
 
 	if (func <= -1)
 	{
