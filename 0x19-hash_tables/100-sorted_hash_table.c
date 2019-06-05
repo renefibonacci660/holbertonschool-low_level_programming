76f24e21d56a06c9a26c285fe10fed34ea1a3337
renefibonacci660 <660@holbertonschool.com>
2019-06-05 03:25:19 +0000

    Created files and updated header for EC #7

---
+++ 0x19-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,62 @@
+#include "hash_tables.h"
+/**
+ * shash_table_create - creates table
+ * @size: size
+ * Return: 0
+**/
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	return (0);
+}
+
+/**
+ * shash_table_set - sets table
+ * @ht: size
+ * @key: key
+ * @value: value
+ * Return: 0
+**/
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	return (0);
+}
+
+/**
+ * shash_table_get - gets table
+ * @ht: pointer to hash table
+ * @key: key
+ * Return: 0
+**/
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	return (0);
+}
+
+/**
+ * shash_table_print - prints table
+ * @ht: pointer to hash table
+ * Return: 0
+ **/
+void shash_table_print(const shash_table_t *ht)
+{
+	return (0);
+}
+
+/**
+ * shash_table_print_rev - prints table in reverse
+ * @ht: pointer to hash table
+ **/
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	return (0);
+}
+
+/**
+ * shash_table_delete - deletes hash table
+ * @ht: pointer to hash table
+ * Return: 0
+ **/
+void shash_table_delete(shash_table_t *ht)
+{
+	return (0);
+}
