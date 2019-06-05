7551d91f0f196587909089da5b625f824f1bfe73
renefibonacci660 <660@holbertonschool.com>
2019-06-05 02:21:46 +0000

    Changed spacing and added notes

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,6 +1,10 @@
 #include "hash_tables.h"
 /**
- * 
+ * key_index - gives index of a key
+ * @key: key
+ * @size: size of the array of the hash table
+ * Return: index at which the key/value pair should be stored
+ * in the array of the hash table
 **/
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
