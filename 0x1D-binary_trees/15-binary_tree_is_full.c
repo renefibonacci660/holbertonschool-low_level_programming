57f4fb7ea88a8806fa34963bf7cafbea8e8d0cc9
renefibonacci660 <660@holbertonschool.com>
2019-07-27 06:07:13 +0000

    checks if there are no leaves

---
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - Checks if binary tree is full
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if its full, 0 otherwise
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
