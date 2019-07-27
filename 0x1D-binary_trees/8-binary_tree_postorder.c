de4c23c371308b59cd4ef514759752474de37f90
renefibonacci660 <660@holbertonschool.com>
2019-07-27 05:28:40 +0000

    post-order traversal of binary tree

---
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - Traverses binary tree using post-order traversal
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+	func(tree->n);
+}
