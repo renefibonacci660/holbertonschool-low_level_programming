c8d9efbcb9cfa6be2c0b4de393640d767e91902d
renefibonacci660 <660@holbertonschool.com>
2019-07-27 05:49:00 +0000

    Returns size of binary tree

---
+++ 0x1D-binary_trees/11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * calculate_size - Calculates the size of a binary tree
+ * @tree: Node representing the root of the tree
+ * Return: Size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (!tree)
+		return (0);
+
+	left += binary_tree_size(tree->left);
+	right += binary_tree_size(tree->right);
+	return (left + right + 1);
+}
