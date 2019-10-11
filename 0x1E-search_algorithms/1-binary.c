092f80c34a0317dd7e89fa3f3e6780b8ae46c624
renefibonacci660 <660@holbertonschool.com>
2019-10-11 13:26:49 -0700

    Uses binary searh to find index value in given array

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+
+/**
+ *binary_search - searches for value in sorted array of ints with
+ * Binary search algo
+ * @array: Array in which to search for the value
+ * @size: Size of the array
+ * @value: Value to search for in array
+ * Return: Index of value if found, else returns -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right;
+	size_t mid, list;
+
+	if (array == NULL)
+		return (-1);
+
+	if (size > 0)
+		right = size - 1;
+	else
+		right = 0;
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (list = left; list <= right; list++)
+		{
+			if (list == right)
+				printf("%i\n", array[list]);
+			else
+				printf("%i, ", array[list]);
+		}
+		mid = (right + left) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (value < array[mid])
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+	return (-1);
+}
